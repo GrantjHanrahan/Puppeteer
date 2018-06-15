839ef1810892723963fe9aa2a7c395ca10b32740
Grant Hanrahan <grantjhanrahan@gmail.com>
2018-06-15 11:55:34 +1000

    added github credentials to run puppeteer

--- index.js
+++ index.js
@@ -1,11 +1,67 @@
 const puppeteer = require('puppeteer');
+const CREDS = require('./creds');
 
 async function run() {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+
   const page = await browser.newPage();
 
-  await page.goto('https://github.com');
-  await page.screenshot({ path: 'screenshots/github.png'});
+  await page.goto('https://github.com/login');
+  // await page.screenshot({ path: 'screenshots/github.png'});
+
+
+  const USERNAME_SELECTOR = '#login_field';
+  const PASSWORD_SELECTOR = '#password';
+  const BUTTON_SELECTOR = '#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block';
+
+  await page.click(USERNAME_SELECTOR);
+  await page.keyboard.type(CREDS.username);
+
+  await page.click(PASSWORD_SELECTOR);
+  await page.keyboard.type(CREDS.password);
+
+  await page.click(BUTTON_SELECTOR);
+
+  await page.waitForNavigation();
+
+  const userToSearch = 'john';
+  const searchUrl = `https://github.com/search?q=${userToSearch}&type=Users&utf8=%E2%9C%93`;
+
+  await page.goto(searchUrl);
+  await page.waitFor(2*1000);
+
+  // const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(1) > div.d-flex > div > a';
+  const LIST_USERNAME_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > a';
+
+  // const LIST_EMAIL_SELECTOR = '#user_search_results > div.user-list > div:nth-child(2) > div.d-flex > div > ul > li:nth-child(2) > a';
+  const LIST_EMAIL_SELECTOR = '#user_search_results > div.user-list > div:nth-child(INDEX) > div.d-flex > div > ul > li:nth-child(2) > a';
+
+  const LENGTH_SELECTOR_CLASS = 'user-list-item';
+
+  let listLength = await page.evaluate((sel) => {
+    return document.getElementsByClassName(sel).length;
+  }, LENGTH_SELECTOR_CLASS);
+
+
+  for( let i = 1; i<= listLength; i++ ){
+    // change the index to the next child
+    let usernameSelector = await page.evaluate((sel) => {
+      let element = document.querySelector(sel);
+      return element? element.innerHTML: null;
+    }, emailSelector);
+
+    // not all users have emails visible
+    if( !email )
+    continue;
+
+    console.log(username, ' -> ', email);
+
+    // TODO save this user
+
+  }
+
 
   browser.close();
 }
